5fdcf379cfdfc4dbdfc9a38cef7e0b95e0bec766
Josemaria <josemarianriagu25@gmail.com>
2019-06-13 05:58:13 +0800

    Add constructor Define state slices Add callbacks to setState Define methods Pass down props

--- friends/src/App.js
+++ friends/src/App.js
@@ -4,24 +4,27 @@ import Friends from './components/Friends';
 import FriendForm from './components/FriendForm';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       friendsList: [],
+      form: {
+        name: "",
+        age: 0,
+        email: ""
+      },
       editMode: false,
-      name: "",
-      age: 0,
-      email: ""
+      friendToEdit: null,
     }
   }
 
   fetchFriendWithAxios = () => {
-    axios.get('http://localhost:5000/friends/')
+    axios.get('http://localhost:5000/friends')
       .then(response => {
-        this.setState({ ...this.state, friendsList: response.data });
+        this.setState(() => ({ friendsList: response.data }));
       })
       .catch(err => {
-        console.log(err.message);
+        return (err.message);
       })
   }
 
@@ -29,36 +32,112 @@ export default class App extends Component {
     this.fetchFriendWithAxios();
   }
 
-  setEdit = () => {
-    this.setState({ ...this.state, editMode: true });
-    console.log("fired!!");
+  inputChange = (field, value) => {
+    this.setState((state) => {
+      return {
+        form: {
+          ...state.form,
+          [field]: value
+        }
+      };
+    });
+  }
+  changeHandler = (event) => {
+    const field = event.target.name;
+    const value = event.target.value;
+    this.inputChange(field, value);
   }
 
-  cNameHandler = event => {
-    this.setState({ ...this.state, name: event.target.value });
+  addFriend = event => {
+    event.preventDefault()
+    const { name, age, email } = this.state.form;
+    const list = this.state.friendsList;
+    const last = list[list.length - 1];
+    const newFriend = {
+      id: last.id + 1,
+      name: name,
+      age: Number(age),
+      email: email
+    }
+    axios.post('http://localhost:5000/friends', newFriend)
+      .then((response) => {
+        this.setState(() => ({
+          friendsList: response.data,
+          form: {
+            name: "",
+            age: 0,
+            email: ""
+          },
+          editMode: false,
+          friendToEdit: null,
+        }))
+      }).catch(err => console.log(err.message))
   }
 
-  cAgeHandler = event => {
-    this.setState({ ...this.state, age: event.target.value });
+  setEdit = (id) => {
+    const friendToEdit = this.state.friendsList.find(friend => friend.id === id);
+    this.setState({
+      ...this.state,
+      form: {
+        name: friendToEdit.name,
+        age: friendToEdit.age,
+        email: friendToEdit.email
+      },
+      editMode: true,
+      friendToEdit,
+    });
   }
 
-  cEmailHandler = event => {
-    this.setState({ ...this.state, email: event.target.value });
+  updateFriend = (event, id) => {
+    event.preventDefault()
+    const { name, age, email } = this.state.form;
+    const updatedFriend = {
+      id: id,
+      name: name,
+      age: age,
+      email: email
+    }
+    axios.put(`http://localhost:5000/friends/${id}`, updatedFriend)
+      .then((response) => {
+        this.setState(() => ({
+          friendsList: response.data,
+          form: {
+            name: "",
+            age: 0,
+            email: ""
+          },
+          editMode: false,
+          friendToEdit: null,
+        }))
+      }).catch(err => console.log(err.message))
+  }
+
+  deleteFriend = id => {
+    axios.delete(`http://localhost:5000/friends/${id}`)
+      .then((response) => {
+        this.setState(() => ({
+          friendsList: response.data,
+        }))
+      }).catch(err => console.log(err.message))
   }
 
   render() {
     return (
       <div className="app-wrapper">
-        <h1>My Buddies</h1>
+        <h1>My Buddies List</h1>
         <section className="app-section">
           <Friends
             setEdit={this.setEdit}
-            friendsList={this.state.friendsList} />
+            deleteFriend={this.deleteFriend}
+            friendsList={this.state.friendsList}
+          />
           <FriendForm
-            cNameHandler={this.cNameHandler}
-            cAgeHandler={this.cAgeHandler}
-            cEmailHandler={this.cEmailHandler}
+            addFriend={this.addFriend}
+            changeHandler={this.changeHandler}
             editMode={this.state.editMode}
+            friendToEdit={this.state.friendToEdit}
+            updateFriend={this.updateFriend}
+            {...this.state.form}
           />
         </section>
       </div>
